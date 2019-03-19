7d96c19f5f2ef7ea5e6a13a1fe01ec2f40287579
David Spörri <davepermen@hotmail.com>
2019-03-20 00:15:01 +0100

    add first version of project

---
+++ Content Pipeline/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IO = System.IO;
+
+namespace Content_Pipeline.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            var files = IO.Directory.GetFiles(@"..\data\original");
+            Files = files;
+        }
+
+        public async Task OnPost(string name, IFormFile file)
+        {
+            if (string.IsNullOrEmpty(name) == false && IO.Path.GetExtension(file.FileName).Substring(1) == "txt")
+            {
+                using (var stream = file.OpenReadStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    IO.File.WriteAllText($@"..\data\original\{name}.txt", await reader.ReadToEndAsync());
+                }
+            }
+            OnGet();
+
+            Process(Files);
+        }
+
+        private void Process(string[] files)
+        {
+            foreach (var input in files)
+            {
+                var output = input.Replace(@"\original\", @"\processed\");
+
+                var contents = IO.File.ReadAllText(input);
+
+                IO.File.WriteAllText(output, contents.ToUpper());
+            }
+        }
+
+        public string[] Files { get; private set; }
+    }
+}
---
+++ Content Pipeline/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Content_Pipeline
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
